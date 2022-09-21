31d3e8b11c2068cbef44a946b1819042d776bfe8
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-09-21 17:52:26 +0200

    Main section skeleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import styles from "./App.module.css";
 import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className={styles["app"]}>
       <Header />
+      <Main />
       <Footer />
     </div>
   );
---
+++ src/components/Main/Main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Main.module.css";
+
+function Main() {
+  return <main></main>;
+}
+
+export default Main;
