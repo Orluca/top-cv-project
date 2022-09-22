8685e206f19b0e775c246d14b7e82e971e570a83
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-09-22 16:53:40 +0200

    Save work data in state

--- src/components/Main/Editor/WorkEditor/WorkEditor.js
+++ src/components/Main/Editor/WorkEditor/WorkEditor.js
@@ -3,24 +3,33 @@ import styles from "./WorkEditor.module.css";
 import Subheader from "../../../General/Subheader/Subheader";
 import WorkCard from "./WorkCard/WorkCard";
 import AddButton from "../../../General/AddButton/AddButton";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
-
-const test = [];
 
 function WorkEditor() {
   const workCard = <WorkCard key={uuidv4()} onInputChange={handleInputChanges} />;
   const [workCards, setWorkCards] = useState([workCard]);
+  const [workData, setWorkData] = useState([]);
 
   function handleAddButton() {
     setWorkCards((prev) => [...prev, workCard]);
   }
 
   function handleInputChanges(val) {
-    if (Object.keys(val).length === 0) return; // Return if object is empty
-    test.push(val);
-    console.log(test);
+    setWorkData((prev) => {
+      const index = prev.findIndex((obj) => obj.id === val.id);
+      if (index === -1) {
+        return [...prev, val];
+      } else {
+        prev[index] = val;
+        return [...prev];
+      }
+    });
   }
+
+  useEffect(() => {
+    console.log(workData);
+  }, [workData]);
 
   return (
     <div>
