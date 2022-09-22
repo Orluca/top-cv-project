2f53c93103784ec5d28981cec934b3389f9e1035
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-09-22 10:02:43 +0200

    Work experience inputs

--- src/components/Main/Editor/WorkEditor/WorkCard/WorkCard.js
+++ src/components/Main/Editor/WorkEditor/WorkCard/WorkCard.js
@@ -1,8 +1,17 @@
 import React from "react";
+import Input from "../../../../General/Input/Input";
+import Textarea from "../../../../General/Textarea/Textarea";
 import styles from "./WorkCard.module.css";
 
 function WorkCard() {
-  return <></>;
+  return (
+    <div className={styles["work-card"]}>
+      <Input name="Role" />
+      <Input name="Company Name" />
+      <Input name="Time Span" />
+      <Textarea name="Description" />
+    </div>
+  );
 }
 
 export default WorkCard;
