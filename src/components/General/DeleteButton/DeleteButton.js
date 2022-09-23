338c42fa01051d9135f1129c9ad541e234ba9506
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-09-23 19:11:59 +0200

    Add button flex

--- src/components/General/DeleteButton/DeleteButton.js
+++ src/components/General/DeleteButton/DeleteButton.js
@@ -1,12 +1,17 @@
 import React from "react";
 import styles from "./DeleteButton.module.css";
+import deleteIcon from "./delete-icon.svg";
 
 function DeleteButton(props) {
   function handleClick() {
     props.onClick();
   }
 
-  return <button onClick={handleClick}>X</button>;
+  return (
+    <button onClick={handleClick} className={styles["delete-btn"]}>
+      <img src={deleteIcon} alt="A delete icon" />
+    </button>
+  );
 }
 
 export default DeleteButton;
