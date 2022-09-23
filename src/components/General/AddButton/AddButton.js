a51905d3a1444c4ecd1387f494669d301f09f19b
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-09-23 19:08:39 +0200

    Make whole add button clickable

--- src/components/General/AddButton/AddButton.js
+++ src/components/General/AddButton/AddButton.js
@@ -3,13 +3,13 @@ import styles from "./AddButton.module.css";
 import plusIcon from "./plus-icon.svg";
 
 function AddButton(props) {
-  function handleButton() {
+  function handleClick() {
     props.onButtonClick();
   }
 
   return (
-    <div className={styles["add-btn-wrapper"]}>
-      <button onClick={handleButton} className={styles["add-btn"]}>
+    <div className={styles["add-btn-wrapper"]} onClick={handleClick}>
+      <button className={styles["add-btn"]}>
         <img src={plusIcon} alt="A plus symbol" />
       </button>
       <div>Add more</div>
