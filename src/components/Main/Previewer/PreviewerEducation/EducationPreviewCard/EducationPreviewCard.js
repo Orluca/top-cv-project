3478685c81b6370d6dad91bc3ecbaf1ed848aece
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-09-24 15:51:00 +0200

    improved education preview card styling

--- src/components/Main/Previewer/PreviewerEducation/EducationPreviewCard/EducationPreviewCard.js
+++ src/components/Main/Previewer/PreviewerEducation/EducationPreviewCard/EducationPreviewCard.js
@@ -4,11 +4,11 @@ import styles from "./EducationPreviewCard.module.css";
 function EducationPreviewCard(props) {
   return (
     <div className={styles["education-preview-card"]}>
-      <div>
+      <div className={styles["education-preview-card__timespan"]}>
         {props.data.timeStart ? props.data.timeStart : "2018"} - {props.data.timeEnd ? props.data.timeEnd : "2020"}
       </div>
-      <h4>{props.data.degree ? props.data.degree : "Degree Name"}</h4>
-      <div>{props.data.university ? props.data.university : "University/School Name"}</div>
+      <h4 className={styles["education-preview-card__degree"]}>{props.data.degree ? props.data.degree : "Degree Name"}</h4>
+      <div className={styles["education-preview-card__university"]}>{props.data.university ? props.data.university : "University/School Name"}</div>
     </div>
   );
 }
