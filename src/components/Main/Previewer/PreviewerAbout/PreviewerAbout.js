13a7245d21116416cf577c59656eee0027cd6ad2
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-09-24 13:35:48 +0200

    Finished about section structure

--- src/components/Main/Previewer/PreviewerAbout/PreviewerAbout.js
+++ src/components/Main/Previewer/PreviewerAbout/PreviewerAbout.js
@@ -6,6 +6,7 @@ function PreviewerAbout(props) {
   return (
     <div>
       <Subheader name="About Me" />
+      <p>{props.data.aboutMe ? props.data.aboutMe : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis rutrum massa adipiscing vel scelerisque. Vulputate magna aliquam et id erat mattis morbi. Pellentesque risus, gravida dui condimentum. Id sit sit id sed tempor duis erat convallis turpis."}</p>
     </div>
   );
 }
