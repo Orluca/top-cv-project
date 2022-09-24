de5379f725b597d0ac1a8d2a47bcb1d66bf6a8c5
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-09-24 15:48:01 +0200

    Improved work preview card styling

--- src/components/Main/Previewer/PreviewerWork/WorkPreviewCard/WorkPreviewCard.js
+++ src/components/Main/Previewer/PreviewerWork/WorkPreviewCard/WorkPreviewCard.js
@@ -4,12 +4,12 @@ import styles from "./WorkPreviewCard.module.css";
 function WorkPreviewCard(props) {
   return (
     <div className={styles["work-preview-card"]}>
-      <div>
+      <div className={styles["work-preview-card__timespan"]}>
         {props.data.timeStart ? props.data.timeStart : "2020"} - {props.data.timeEnd ? props.data.timeEnd : "2022"}
       </div>
-      <h4>{props.data.role ? props.data.role : "Role"}</h4>
-      <div>{props.data.company ? props.data.company : "Company"}</div>
-      <p>{props.data.description ? props.data.description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit mus venenatis nunc, cursus cursus velit."}</p>
+      <h4 className={styles["work-preview-card__role"]}>{props.data.role ? props.data.role : "Role"}</h4>
+      <div className={styles["work-preview-card__company"]}>{props.data.company ? props.data.company : "Company"}</div>
+      <p className={styles["work-preview-card__description"]}>{props.data.description ? props.data.description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit mus venenatis nunc, cursus cursus velit."}</p>
     </div>
   );
 }
